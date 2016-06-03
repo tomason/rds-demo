cb88cf4156f6cbfc36a8caaff9edb7cee6c22a5c
Tomas Schlosser <tschloss@redhat.com>
2016-06-03 10:38:29 +0200

    Demo application

---
+++ client/src/main/java/com/redhat/ContinuousExample.java
@@ -0,0 +1,53 @@
+package com.redhat;
+
+import org.kie.api.KieServices;
+import org.kie.api.command.Command;
+import org.kie.api.command.KieCommands;
+import org.kie.api.runtime.ExecutionResults;
+import org.kie.server.api.marshalling.MarshallingFormat;
+import org.kie.server.api.model.ServiceResponse;
+import org.kie.server.client.KieServicesClient;
+import org.kie.server.client.KieServicesConfiguration;
+import org.kie.server.client.KieServicesFactory;
+import org.kie.server.client.RuleServicesClient;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ContinuousExample {
+	private static final String ENDPOINT = "http://kie-app-cruyff-rds-training.apps.devel.xpaas/kie-server/services/rest/server";
+	private static final String USERNAME = "kieserver";
+	private static final String PASSWORD = "yyvVpk6!";
+
+	public static void main(String[] args) {
+		KieServicesConfiguration config = KieServicesFactory.newRestConfiguration(ENDPOINT, USERNAME, PASSWORD);
+		config.setMarshallingFormat(MarshallingFormat.JSON);
+		config.setTimeout(300_000L);
+
+		KieServicesClient client = KieServicesFactory.newKieServicesClient(config);
+		RuleServicesClient rules = client.getServicesClient(RuleServicesClient.class);
+
+		KieCommands cmdFactory = KieServices.Factory.get().getCommands();
+
+		List<Command<?>> commands = new LinkedList<>();
+		commands.add(cmdFactory.newInsert("Tomas Schlosser"));
+		commands.add(cmdFactory.newInsert(new LinkedList<String>(), "list"));
+		commands.add(cmdFactory.newFireAllRules());
+
+		while (true) {
+			ServiceResponse<ExecutionResults> response = rules.executeCommandsWithResults("DemoContainer", cmdFactory.newBatchExecution(commands, "DemoProjectSession"));
+			System.out.println(response.getMsg());
+
+			List<String> results = (List) response.getResult().getValue("list");
+			System.out.println("Results:");
+			results.forEach(result -> System.out.println("  " + result));
+
+			try {
+				Thread.sleep(2_000L);
+			} catch (InterruptedException ex) {
+				// interrupted, end the program
+				break;
+			}
+		}
+	}
+}
